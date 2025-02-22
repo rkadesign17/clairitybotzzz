6e1f0dfd21f99c910f2c52c99f90155ae269833a
rkadesign17 <rkaripkinabila@gmail.com>
2025-02-23 00:27:52 +0700

    ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
 global.ownername = 'R,K,A DESIGN';
-global.owner = '6283804078729';
+global.owner = '6289656277900';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
